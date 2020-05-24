05c1ea6341091ec649f2110cefaea30b1d0048a3
rishav1006115 <65595963+rishav1006115@users.noreply.github.com>
2020-05-24 15:13:32 +0530

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -2,12 +2,18 @@ import random
 
 def primary():
   #print("Keep it logically awesome.")
- last = 13
-  rnd = random.randint(0, last)
-  f = open("quotes.txt")
-  quotes = f.readlines()
-  f.close()
-print(quotes[rnd])
+ #last = 13
+  #rnd = random.randint(0, last)
+  #f = open("quotes.txt")
+  #quotes = f.readlines()
+  #f.close()
+#print(quotes[rnd])
+a=21%2
+b=21/2
+c=21//2
+print(a)
+print(b)
+print(c)
 
 if __name__== "__main__":
   primary()
